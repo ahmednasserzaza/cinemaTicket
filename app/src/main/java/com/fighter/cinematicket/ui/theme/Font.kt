39954153605e1bbdc6eb5edbc848f8fd3e4b9c27
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-07-02 19:01:16 +0300

    add fonts

---
+++ app/src/main/java/com/fighter/cinematicket/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.fighter.cinematicket.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.fighter.cinematicket.R
+
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_regular, weight = FontWeight.Normal),
+    Font(R.font.open_sans_medium, weight = FontWeight.Medium),
+)
